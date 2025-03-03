194dde339a39d9b10a98f8603b7e0d3f91da4f97
Cristian6597 <cristianercolani6597@gmail.com>
2025-03-03 16:55:24 +0100

    Collegato al front, aggiunto CORS, aggiunta funzione filtro per data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import express from "express";
 import transactionRouter from "./routes/transaction.route.js";
+import cors from "cors";
+
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", //indirizzo del server
+    methods: "*", //metodi che accetta
+  })
+);
 app.use(express.json());
 app.use(transactionRouter);
 
--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -4,10 +4,43 @@ import prisma from "../prisma/prismaClient.js";
 const transactionRouter = express.Router();
 
 transactionRouter.get("/transactions", async (req, res) => {
+  //passa il parametro nel url
+
+  const { month } = req.query;
   try {
+    //divide il parametro in due parti
+    let [year, monthNumber] = month.split("-");
+
+    //converte il mese in numero
+    let parsedMonth = parseInt(monthNumber) - 1;
+    let parsedYear = parseInt(year);
+
+    //controlla se il mese è valido
+    if (parsedMonth < 0 || parsedMonth > 11) {
+      return res
+        .status(400)
+        .json({ error: "Mese non valido. Deve essere tra 01 e 12." });
+    }
+
+    //aggiunge un if in modo che quando devo visuallizare le transazioni di dicembre, prenda le transazioni tra dicembre e gennaio dell'anno successivo
+    console.log(typeof parsedMonth);
+    if (monthNumber === 11) {
+      const nextYear = parseInt(year) + 1; // Passa all'anno successivo
+      year = nextYear; // Aggiorna l'anno
+
+      // Modifica il mese per prendere gennaio (01)
+      monthNumber = 0;
+    }
+    console.log(year);
     const transactions = await prisma.transactions.findMany({
       orderBy: {
         date: "desc",
+      },
+      where: {
+        date: {
+          gte: new Date(Date.UTC(parsedYear, parsedMonth, 1)), // Inizio del mese
+          lt: new Date(Date.UTC(parsedYear, parsedMonth + 1, 1)), // Inizio del mese successivo
+        },
       },
     });
     res.json(transactions);
